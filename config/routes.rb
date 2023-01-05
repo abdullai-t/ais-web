9789abf59f52b7623eb0d8ae1638761f7db9c97a
Tahiru <abdullai.tahiru@gmail.com>
2023-01-05 16:17:54 +0000

    more tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
+  devise_scope :user do  
+   get '/users/sign_out' => 'devise/sessions#destroy'
+   post "/users/sign_up"=>'devise/registrations#create'
+   end
   resources :students
-  root "auth#index"
+  root "students#index"
 end
