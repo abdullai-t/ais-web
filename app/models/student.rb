1c63eb55c75fd12702b3511af16d7897842464bd
Tahiru <abdullai.tahiru@gmail.com>
2023-01-12 11:50:10 +0000

    added field validations

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,10 @@ class Student < ApplicationRecord
     before_validation :set_defaults
     has_one_attached:image
 
+    validates :first_name, presence: true
+    validates :contact, presence: true
+    validates :surname, presence: true
+
     def set_defaults
         self.ref_number = SecureRandom.alphanumeric(7).upcase()
         self.enrolled_at = DateTime.current.to_date
